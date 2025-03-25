7d1f4d7c5e31cfda9dd1c0370bb8093636a733ce
ThembaMthimunye <218634567@tut4life.ac.za>
2025-03-25 16:33:05 +0200

    qwerty

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,12 +1,12 @@
 import useGetMessages from "@/hooks/useGetMessages";
 import Message from "./Message";
-
+import MessageSkeleton from "@/Skeletons/MessageSkeleton";
 const Messages = () => {
 	const {messages,loading}=useGetMessages();
 	console.log("message",messages)
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
-			{loading&&}
+			{loading&&[...Array(3)].map((_,idx)=><MessageSkeleton key={idx}/>)}
 		</div>
 	);
 };
