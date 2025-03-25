085432520de120df92aa51d4099bfdad60d25338
ThembaMthimunye <218634567@tut4life.ac.za>
2025-03-25 16:22:23 +0200

    we are there almost

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,12 @@
+import useGetMessages from "@/hooks/useGetMessages";
 import Message from "./Message";
 
 const Messages = () => {
+	const {messages,loading}=useGetMessages();
+	console.log("message",messages)
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
+			{loading&&}
 		</div>
 	);
 };
