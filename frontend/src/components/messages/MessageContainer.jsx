2ee08fb3fa3568e827748c2c7efd32b00142cc30
ThembaMthimunye <218634567@tut4life.ac.za>
2025-03-27 16:24:17 +0200

    75% done

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -34,12 +34,13 @@ const MessageContainer = () => {
 };
 
 export default MessageContainer;
+const user=JSON.parse(localStorage.getItem("user"))
 
 const NoChatSelected = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-800 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋  ❄</p>
+        <p>Welcome 👋 {user.name}</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
